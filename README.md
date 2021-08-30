654df219c792c5083b9325c01c6b3beabb10ac43
1BObC <bob.cocksedge@gmail.com>
2021-08-30 22:30:33 +0100

    Revised string operations

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 First Python exercises
 "Set up files"
 "Complete python.land introduction-to-python to start of functions"
+"Revised string operations"
--- ersterP.py
+++ ersterP.py
@@ -1,2 +1,18 @@
+# Three ways to print a string statement.
+msg = "Hello to us"
+print(msg)
 
-print("Hello Pythons")
+msga= "try number 2"
+print(msga)
+
+msgb= "try number 3"
+print(msgb)
+
+#Create multi-line strings
+bigmsg= """Line 1,
+... Line 2,
+...Line 3."""
+print(bigmsg)
+
+#String operations
+print(msgb.upper())
