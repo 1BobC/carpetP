113b8983a65dac996cc7b4a217cee2c3de824196
1BObC <bob.cocksedge@gmail.com>
2021-10-02 16:49:30 +0100

    Completed defining functions

--- vierte.py
+++ vierte.py
@@ -59,34 +59,82 @@
  
  #practise basic data types
  #find types
- x = 42
- y = 3 / 4
- z = int('7')
- a = float(5)
-name = "Nina"
+#  x = 42
+#  y = 3 / 4
+#  z = int('7')
+#  a = float(5)
+# name = "Nina"
 
-#division
- rent = 480
->>> per_day = rent / 30
->>> print(per_day)
-16.0
+# #division
+#  rent = 480
+# >>> per_day = rent / 30
+# >>> print(per_day)
+# 16.0
 
-#three ways of printing strings
-print("Hello world")
-Hello world
-name = "Nina"
-print("My name is", name)
-My name is Nina
+# #three ways of printing strings
+# print("Hello world")
+# Hello world
+# name = "Nina"
+# print("My name is", name)
+# My name is Nina
 
-name = "Nina"
-print("Hello, my name is %s" % name)
-Hello, my name is Nina
+# name = "Nina"
+# print("Hello, my name is %s" % name)
+# Hello, my name is Nina
     
-name = "Nina"
-print(f"Hello, my name is {name} and I pay ${rent / 30} in rent per day")
-Hello, my name is Nina and I pay $16.0 in rent per day   
+# name = "Nina"
+# print(f"Hello, my name is {name} and I pay ${rent / 30} in rent per day")
+# Hello, my name is Nina and I pay $16.0 in rent per day   
 
-#helper functions 
-dir()
+# #helper functions 
+# dir()
 
-#functions see definitions
+# #functions see definitions
+# # A Basic Function that accepts no arguments and returns nothing.
+# def hello_world():
+#     print("Hello World")
+        
+# # A Function that accepts two arguments, and returns the value of
+# # those numbers added together.
+# def mult_numbers(x, y):
+#     return x * y
+    
+#Exercises on return statement:
+#no return stmnt
+#  def foo():
+#      x = 5
+
+#  val = foo()
+#  type(val)
+# <type 'NoneType'>
+
+# #return stmnt no value
+# def foo():
+#     ...     x = 5
+# ...     return
+# ...
+#  val = foo()
+#  type(val)
+# <type 'NoneType'>
+
+# #return stmnt with value
+# def oof():
+#     x = 5
+#     return x
+
+# val = foo()
+# type(val)
+# <type 'NoneType'>
+
+# #importance of indentation
+# >>> def add_numbers(x, y):
+# ... return x + y
+# ...
+
+#storing and using a return value
+>>> def add_numbers(x, y):
+...     return x + y
+...
+>>> new_number = add_numbers(3, 5)
+>>> new_number
+8
